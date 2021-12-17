0f946b0a06180041eddbe3e083440ca984e807c9
mohammad-hosein-shahpouri <mh.shahpouri@gmail.com>
2021-12-17 09:43:56 +0000

    Deploying to gh-pages from @ mohammad-hosein-shahpouri/RequestInfo@3bbe7979b65e48ea46f468efe98ad2a2ab728cb8 🚀

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   "/RequestInfo/earth-dark.svg",
   "/RequestInfo/earth-light.svg",
   "/RequestInfo/static/js/2.43a1e59c.chunk.js",
-  "/RequestInfo/static/js/main.c36798b4.chunk.js",
+  "/RequestInfo/static/js/main.678eecdb.chunk.js",
   "/RequestInfo/static/css/2.9d3f7eb1.chunk.css",
   //Font
   "https://fonts.googleapis.com/css2?family=Allerta&family=Archivo:wght@500;700&display=swap",
